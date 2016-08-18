ad23ee8c1195ac6ec4e14297df808d282c3599ce
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-08-18 10:03:09 +0200

    Mount user storage on session start
    create a team page in user section
    
    user can now monitor his team user status
    
    user can create a team
    
    team is working
    
    fix relationship error by merging backend and frontend
    
    Teams added
    
    fix icon positioning
    
    Add an option in configuration page that allows admin to enable the team feature
    
    hide team modal when team enabled is off
    
    jshint compliant
    
    fix css
    
    prevent team popup in configuration page
    
    setupController hook in user/index/route set the model to undefined so we could not retrieve the use list
    
    solve rebase that prevent backend from starting
    
    ensure the backend return the correct relationship. ensure the frontend manage the data correctly
    
    add options to input-confirm 'enter' and 'disableControls' options
    
    redirect user to team page when he just logged in and doesn't have a team yet
    
    create team page
    
    adjust icon position in machine page
    
    fix a bug. user list was not retrieved
    
    make the checkbox in configuration page easier tot enable and disable
    
    remove the team modal
    
    add validation for team creation
    
    fix rebase mistake
    
    bugfix: a regular user doesn't see anything in team page
    
    add licensing and fix jshint errors
    
    fix a bug that prevent user to craate a team. the confirm input component did not save value when disabledControl option is activated
    
    fix wording
    
    fix a bug that reload page when user create a team

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -25,7 +25,82 @@ const Promise = require('bluebird');
 const _ = require('lodash');
 const querystring = require('querystring');
 
-/* globals App, MachineService, JsonApiService, PlazaService, StorageService, OwncloudService */
+/* globals App, MachineService, JsonApiService, PlazaService, OwncloudService */
+/* globals StorageService */
+
+function mountUserStorage(machine, user) {
+  return StorageService.findOrCreate(user)
+  .then((storage) => {
+    return PlazaService.exec(machine.ip, machine.plazaport, {
+      command: [
+        `C:\\Windows\\System32\\net.exe`,
+        'use',
+        'z:',
+        `\\\\${storage.hostname}\\${storage.username}`,
+        `/user:${storage.username}`,
+        storage.password
+      ],
+      wait: true,
+      hideWindow: true,
+      username: machine.username
+    })
+    .then(() => {
+      return PlazaService.exec(machine.ip, machine.plazaport, {
+        command: [
+          `C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`,
+          '-Command',
+          '-'
+        ],
+        wait: true,
+        hideWindow: true,
+        username: machine.username,
+        stdin: '$a = New-Object -ComObject shell.application;$a.NameSpace( "Z:\" ).self.name = "Storage"'
+      });
+    })
+    .catch(() => {
+      // User storage is probably already mounted
+      console.error('Unable to mount user storage');
+    });
+  });
+}
+
+function launchApp(machine, application) {
+  return PlazaService.exec(machine.ip, machine.plazaport, {
+    command: [
+      application.filePath
+    ],
+    username: machine.username
+  });
+}
+
+function mountTeamStorage(machine, user) {
+  return OwncloudService.getStorageForUser(user)
+  .then((webdav) => {
+
+    let username = querystring.unescape(webdav.username);
+    let password = querystring.unescape(webdav.password);
+
+    return PlazaService.exec(machine.ip, machine.plazaport, {
+      command: [
+        'C:\\Windows\\System32\\net.exe',
+        'use',
+        'Y:',
+        webdav.url,
+        `/user:${username}`,
+        '/persistent:no',
+        password,
+      ],
+      username: machine.username,
+      'hide-window': true,
+      wait: true
+    });
+  })
+  .catch((/* err */) => {
+    // console.error(err);
+    console.error('Unable to mount Owncloud storage');
+  });
+}
+
 
 /**
  * Controller of apps resource.
@@ -188,7 +263,6 @@ module.exports = {
                 });
             });
         }
-
         return res.ok(application);
       })
       .catch((err) => {
